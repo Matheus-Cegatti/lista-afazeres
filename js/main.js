9f3f74382c3c882dffb3f68cf457adaa1169efeb
Matheus-Cegatti <cegattimatheus@gmail.com>
2022-09-02 18:29:42 -0300

    dando funcionalidade a opção de editar

--- js/main.js
+++ js/main.js
@@ -1,11 +1,12 @@
 const caixaDeTexto = document.querySelector("#afazeres-input");
 const formularioTexto = document.querySelector("#formulario-lista");
-const listaTarefas = document.querySelector(".lista-afazeres");
+const listaTarefas = document.querySelector("#lista-afazeres");
 const editarTarefas = document.querySelector("#editar-fromulario");
 const inputEditar = document.querySelector("#input-editar");
 const cancelarEdicao = document.querySelector(".cancelar-edicao");
-const feitoBotao = document.querySelector(".botao-feito");
-const listaFeita = document.querySelectorAll(".lista");
+// const feitoBotao = document.querySelector(".botao-feito");
+// const listaFeita = document.querySelectorAll(".lista");
+let tituloAntigoDigitado;
 
 //criando a div com as tarefas digitada
 const salvarLista  = (texto) => {
@@ -44,9 +45,24 @@ const salvarLista  = (texto) => {
 
 //escondendo a parte de adicionar tarefas e tarefas adicionadas quando clica em editar e mostrando a parte do edit e fazendo o caminho oposto quando se clica em no botao de edit
 const alternandoLayout = () => {
+    editarTarefas.classList.toggle("esconder");
     formularioTexto.classList.toggle("esconder");
     listaTarefas.classList.toggle("esconder");
-    editarTarefas.classList.toggle("esconder");
+}
+
+
+
+const atualizarEdicao = (inputDeEditar) => {
+    const listas = document.querySelectorAll(".lista");
+
+    //selecionando todos itens de minha lista adicionado
+    listas.forEach((lista) => {
+        let tituloDigitado = lista.querySelector("h3");
+        //verificando se o texto do meu titulo é igual ao valor antigo digitado
+        if(tituloDigitado.innerText === tituloAntigoDigitado) {
+            tituloDigitado.innerText = inputDeEditar //e aqui eu substituo o texto antigo armazenado pelo novo texto
+        }
+    })
 }
 
 //pegando a informação digitada no input
@@ -68,6 +84,14 @@ formularioTexto.addEventListener("submit", (evento) => {
 document.addEventListener("click", (evento) => {
     const elementoAlvo = evento.target;
     const elementoParente = elementoAlvo.closest("div") //buscando a div mais proxima da const elementeAlvo
+    let tituloDigitado;
+
+
+    //buscando se o elemento que vamos clicar é um titulo e se esse titulo é um h3, dizendo depois que o tituloDigitado (clicado) receber o seu valor de texto
+    if(elementoParente && elementoParente.querySelector("h3")) {
+        tituloDigitado = elementoParente.querySelector("h3").innerText || ""; //esta buscando o texto digitado quando clico em editar
+        // console.log(tituloDigitado);
+    }
 
     if(elementoAlvo.classList.contains("botao-feito")) {
         elementoParente.classList.toggle("feito")
@@ -77,7 +101,11 @@ document.addEventListener("click", (evento) => {
     //encontrando o botão de editar
     if(elementoAlvo.classList.contains("botao-editar")) {
         alternandoLayout()
-        console.log("edit");
+        //guardando o "valor" digitado quando clico em editar -- deixando o campo preenchido quando clicado na caneta de editar
+        inputEditar.value = tituloDigitado;
+        console.log(tituloDigitado);
+        tituloAntigoDigitado = tituloDigitado;
+        console.log(tituloAntigoDigitado);
     }
 
 
@@ -86,4 +114,27 @@ document.addEventListener("click", (evento) => {
         elementoParente.remove();
         console.log("Excluiu");
     }
+})
+
+
+//dando funcionalidade ao botão de cancelar edição e alternando a div
+cancelarEdicao.addEventListener("click", (evento) => {
+    evento.preventDefault();
+    alternandoLayout();
+
+});
+
+
+//quando clicado no botão de submit ele envia o novo valor (troca o valor) substituindo e editando os textos
+editarTarefas.addEventListener("submit", (evento) => {
+    evento.preventDefault(); //caso esteja vazio ele cancela a edição
+
+    const inputDeEditar = inputEditar.value;
+
+    if(inputDeEditar) {
+        atualizarEdicao(inputDeEditar)
+    }
+
+    alternandoLayout()
+    console.log("EDIÇÃO 2");
 })
